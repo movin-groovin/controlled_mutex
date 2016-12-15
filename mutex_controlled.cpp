240d7c59a963d317fc992eab22801d4cbd541bcd
Yaroslav Neusypov <yaneusypov@2reallife.com>
2016-12-15 18:38:02 +0300

    some changes

--- mutex_controlled.cpp
+++ mutex_controlled.cpp
@@ -9,8 +9,8 @@ namespace mutex_controlled
 	//
 	
 #ifndef NDEBUG
-	void check_with_assert() { ccounted_mutexes_strategy<>::check_with_assert(); }
-	void check_with_exception () { ccounted_mutexes_strategy<>::check_with_exception(); }
+	void check_with_assert() { ccounted_mutexes_strategy::check_with_assert(); }
+	void check_with_exception () { ccounted_mutexes_strategy::check_with_exception(); }
 #else
 	void check_with_assert() { ; }
 	void check_with_exception () { ; }
